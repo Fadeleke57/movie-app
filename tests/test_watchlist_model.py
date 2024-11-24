08c22f479fc49a70e0917f44cfa0e735330731a6
Callie Liu <liu.callie04@gmail.com>
2024-11-24 15:02:44 -0500

    fix test but still is receiving errors

--- tests/test_watchlist_model.py
+++ tests/test_watchlist_model.py
@@ -97,10 +97,12 @@ def test_add_movie_to_watchlist_user_not_found(mock_user_query):
         add_movie_to_watchlist("non_existent_user", "tt1234567")
 
 #Test if adding was usuccessful because movie not found
-def test_add_movie_to_watchlist_movie_not_found(mock_fetch_movie_by_id):
-    mock_fetch_movie_by_id.return_value = None  
-    with pytest.raises(ValueError, match="Movie not found"):
-        add_movie_to_watchlist("test_user", "tt1234567")
+def test_add_movie_to_watchlist_movie_not_found(mock_fetch_movie_by_id, mock_user_query, mock_db_session, app):
+    mock_fetch_movie_by_id.return_value = None
+    with app.app_context():
+        mock_user_query.filter_by.return_value.first.return_value = Mock(id=1, username="test_user")
+        with pytest.raises(ValueError, match="Movie not found"):
+            add_movie_to_watchlist("test_user", "tt1234567")
 
 #Test add duplicates
 def test_add_movie_already_in_watchlist(mock_user_query, mock_fetch_movie_by_id, mock_watchlist_query):
