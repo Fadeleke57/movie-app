3fae89c1c9e2d4a796e41ccc348788f4cd9432e0
Callie Liu <liu.callie04@gmail.com>
2024-11-24 18:44:31 -0500

    added update watchlist route

--- app/routes/watchlist_routes.py
+++ app/routes/watchlist_routes.py
@@ -6,7 +6,7 @@ from app.models.user import User
 from app.utils.watchlist_utils import (
     add_movie_to_watchlist,
     delete_movie_from_watchlist,
-
+    update_movie_from_watchlist
 )
 
 watchlist_bp = Blueprint('watchlist', __name__)
@@ -88,7 +88,7 @@ def delete_from_watchlist():
         if not user:
             return jsonify({'error': 'User not found'}), 404
 
-        #add movie to watchlist 
+        #delete movie to watchlist 
         delete_movie_from_watchlist(username=username, imdb_id=imdb_id)
         logger.info(f"Movie {imdb_id} deleted from {username}'s watchlist")
         return jsonify({"message": f"Movie deleted from {username}'s watchlist"}), 200
@@ -98,5 +98,54 @@ def delete_from_watchlist():
         logger.error(f"Unexpected error: {str(e)}")
         return jsonify({"error": "An unexpected error occurred"}), 500
     
+@watchlist_bp.route('/update-watchlist', methods=['PUT'])
+def update_watchlist():
+    """
+    Route to update a watchlist for a user.
+    
+    Expects JSON Input:
+        - username(str): username of user
+        - imdb_id(str): IMDb ID of movie
+        - watch_state(str): watching state is either 'To Watch' or 'Watched' or 'Watch Next'
+    
+    Returns:
+        JSON response indicating the success of deleting a watchlist.
 
+    Raises:
+        400 error if invalid input
+        404 error if username not found
+        404 error if watching state not 'To Watch' or 'Watched' or 'Watch Next'
+        500 error if unexcepted error occurred
+    """
+    logger.info("Updating watchlist")
+    
+    try:
+        data = request.get_json()
+        username = data.get('username')
+        imdb_id = data.get('imdb_id')
+        watching_state = data.get('watching_state')
+
+        #check if valid input
+        if not username or not imdb_id or not watching_state:
+            return jsonify({'error': 'Username, imdb_id, and watching_state required'}), 400
+
+        #check if user exists
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+        
+        #check if watching state is not 'To Watch' or 'Watched' or 'Watch Next'
+        if watching_state not in ["To Watch", "Watched", "Watch Next"]:
+            return jsonify({'error': 'Watching state must be "To Watch" or "Watched" or "Watch Next"'}), 404
+
+        #update movie in watchlist 
+        update_movie_from_watchlist(username=username, imdb_id=imdb_id, new_state=watching_state)
+        logger.info(f"Movie {imdb_id} updated from {username}'s watchlist")
+        return jsonify({"message": f"Movie updated from {username}'s watchlist"}), 200
+        
+    #Handle unexpected errors
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+    
                 
