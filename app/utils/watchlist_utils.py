787194a3b72d2ca63714cb4ffd0afe2d045b5ab3
Callie Liu <liu.callie04@gmail.com>
2024-11-24 18:30:05 -0500

    add update watchlist function

--- app/utils/watchlist_utils.py
+++ app/utils/watchlist_utils.py
@@ -76,4 +76,47 @@ def delete_movie_from_watchlist(username, imdb_id):
 
     logger.info(f"Deleted movie '{watchlist_entry.title}' from {username}'s watchlist successfully.")
 
+def update_movie_from_watchlist(username, imdb_id, new_state):
+    """
+    Updates a movie and its watching state from user's watchlist.
+    Deletes movie if state is updated to watched.
+
+    Arguments:
+        username (str): The username of user.
+        imdb_id (str): The IMDb of the movie.
+
+    Raises:
+        ValueError: If user is not found.
+        ValueError: If watchlist entry is not found.
+        ValueError: If watch state is not 'To Watch' or 'Watched' or 'Watch Next'.
+    """
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        raise ValueError("User not found")
+
+    watchlist_entry = Watchlist.query.filter_by(user_id=user.id, imdb_id=imdb_id).first()
+    if not watchlist_entry:
+        raise ValueError("Watchlist entry not found")
     
+    if new_state not in ["To Watch", "Watched", "Watch Next"]:
+        raise ValueError("New state must be 'To Watch' or 'Watched' or 'Watch Next'")
+    
+    if new_state == "Watched":
+        delete_movie_from_watchlist(username, imdb_id)
+ 
+    if new_state == "Watch Next":
+        movie = fetch_movie_by_id(imdb_id)
+        watchlist_entry.user_id = user.id,
+        watchlist_entry.title = movie.get('title'),
+        watchlist_entry.imdb_id=movie.get('imdb_id'),
+        watchlist_entry.year=movie.get('year'),
+        watchlist_entry.rated=movie.get('rated'),
+        watchlist_entry.runtime=movie.get('runtime'),
+        watchlist_entry.plot=movie.get('plot'),
+        watchlist_entry.genre=movie.get('genre'),
+        watchlist_entry.imdb_rating=movie.get('imdb_rating'),
+        watchlist_entry.type=movie.get('type'),
+        watchlist_entry.watching_state="Watch Next" 
+    
+    db.session.commit()
+    logger.info(f"Updated '{watchlist_entry.title}' for {username}")
