bc59674a8f3e4497a81946dbc762294fcc5e8c89
Callie Liu <liu.callie04@gmail.com>
2024-11-24 17:30:05 -0500

    add delete route

--- app/routes/watchlist_routes.py
+++ app/routes/watchlist_routes.py
@@ -4,7 +4,9 @@ import os
 from app.routes.user_routes import logger
 from app.models.user import User
 from app.utils.watchlist_utils import (
-    add_movie_to_watchlist
+    add_movie_to_watchlist,
+    delete_movie_from_watchlist,
+
 )
 
 watchlist_bp = Blueprint('watchlist', __name__)
@@ -37,7 +39,7 @@ def add_to_watchlist():
         if not username or not imdb_id:
             return jsonify({'error': 'Username and imdb_id required'}), 400
 
-         #check if user exists
+        #check if user exists
         user = User.query.filter_by(username=username).first()
         if not user:
             return jsonify({'error': 'User not found'}), 404
@@ -47,10 +49,54 @@ def add_to_watchlist():
         logger.info(f"Movie {imdb_id} added to {username}'s watchlist")
         return jsonify({"message": f"Movie added to {username}'s watchlist"}), 201
         
-    # Handle unexpected errors
+    #Handle unexpected errors
     except Exception as e:
         logger.error(f"Unexpected error: {str(e)}")
         return jsonify({"error": "An unexpected error occurred"}), 500
     
 
+@watchlist_bp.route('/delete-from-watchlist', methods=['DELETE'])
+def delete_from_watchlist():
+    """
+    Route to delete a watchlist for a user.
+    
+    Expects JSON Input:
+        - username(str): username of user
+        - imdb_id(str): IMDb ID of movie
+    
+    Returns:
+        JSON response indicating the success of deleting a watchlist.
+
+    Raises:
+        400 error if invalid input
+        404 error if username not found
+        500 error if unexcepted error occurred
+    """
+    logger.info("Deleting from watchlist")
+    
+    try:
+        data = request.get_json()
+        username = data.get('username')
+        imdb_id = data.get('imdb_id')
+
+        #check if valid input
+        if not username or not imdb_id:
+            return jsonify({'error': 'Username and imdb_id required'}), 400
+
+        #check if user exists
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+
+        #add movie to watchlist 
+        delete_movie_from_watchlist(username=username, imdb_id=imdb_id)
+        logger.info(f"Movie {imdb_id} deleted from {username}'s watchlist")
+        return jsonify({"message": f"Movie deleted from {username}'s watchlist"}), 200
         
+    #Handle unexpected errors
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        return jsonify({"error": "An unexpected error occurred"}), 500
+    
+
+                
