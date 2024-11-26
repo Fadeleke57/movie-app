d73ea9bf3fdf5a7222d02b6c938ddb8f93e791dd
Callie Liu <liu.callie04@gmail.com>
2024-11-26 14:08:18 -0500

    fix add and delete to work with smoketest

--- app/utils/watchlist_utils.py
+++ app/utils/watchlist_utils.py
@@ -27,6 +27,8 @@ def add_movie_to_watchlist(username, imdb_id):
     if not movie:
         raise ValueError("Movie not found")
     
+    print(movie.keys())
+
     existing_entry = Watchlist.query.filter_by(user_id=user.id, imdb_id=imdb_id).first()
     if existing_entry:
         raise ValueError(f"Movie '{movie.get('title')}' is already in {username}'s watchlist.")
@@ -34,15 +36,15 @@ def add_movie_to_watchlist(username, imdb_id):
     
     watchlist = Watchlist(
         user_id = user.id,
-        title = movie.get('title'),
-        imdb_id=movie.get('imdb_id'),
-        year=movie.get('year'),
-        rated=movie.get('rated'),
-        runtime=movie.get('runtime'),
-        plot=movie.get('plot'),
-        genre=movie.get('genre'),
-        imdb_rating=movie.get('imdb_rating'),
-        type=movie.get('type'),
+        title = movie.get('Title'),
+        imdb_id=movie.get('imdbID'),
+        year=movie.get('Year'),
+        rated=movie.get('Rated'),
+        runtime=movie.get('Runtime'),
+        plot=movie.get('Plot'),
+        genre=movie.get('Genre'),
+        imdb_rating=movie.get('imdbRating'),
+        type=movie.get('Type'),
         watching_state="To Watch" 
     )
 
@@ -108,15 +110,15 @@ def update_movie_from_watchlist(username, imdb_id, new_state):
     if new_state == "Watch Next":
         movie = fetch_movie_by_id(imdb_id)
         watchlist_entry.user_id = user.id
-        watchlist_entry.title = movie.get('title')
-        watchlist_entry.imdb_id=movie.get('imdb_id')
-        watchlist_entry.year=movie.get('year')
-        watchlist_entry.rated=movie.get('rated')
-        watchlist_entry.runtime=movie.get('runtime')
-        watchlist_entry.plot=movie.get('plot')
-        watchlist_entry.genre=movie.get('genre')
-        watchlist_entry.imdb_rating=movie.get('imdb_rating')
-        watchlist_entry.type=movie.get('type')
+        watchlist_entry.title = movie.get('Title')
+        watchlist_entry.imdb_id=movie.get('imdbID')
+        watchlist_entry.year=movie.get('Year')
+        watchlist_entry.rated=movie.get('Rated')
+        watchlist_entry.runtime=movie.get('Runtime')
+        watchlist_entry.plot=movie.get('Plot')
+        watchlist_entry.genre=movie.get('Genre')
+        watchlist_entry.imdb_rating=movie.get('imdbRating')
+        watchlist_entry.type=movie.get('Type')
         watchlist_entry.watching_state="Watch Next" 
     
     db.session.commit()
